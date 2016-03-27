1618aa7c20fb81b8fea3c5887cdad3575d95523b
Thomas Chatting <tom.chatting@live.co.uk>
2016-03-27 11:37:30 +0100

    Removed repeating blocks
    + added $url variable to remove repeating blocks of str_replace

--- square/square.inc.php
+++ square/square.inc.php
@@ -49,13 +49,14 @@ Class Square {
     }
 
     $domain = preg_replace('#^www\.#', '', $_SERVER['SERVER_NAME']);
+    $url = str_replace('index.php', '', 'http://'.$domain.$_SERVER['PHP_SELF']);
     self::$site['title'] = $setting['site_name'];
     self::$site['headline'] = $setting['tagline'];
 
     # check to see if we're using htaccess and add a ?, otherwise spit out the url
-    self::$site['url'] = str_replace('index.php', '', file_exists('.htaccess') ? 'http://'.$domain.$_SERVER['PHP_SELF'] : 'http://'.$domain.$_SERVER['PHP_SELF'].'?/');
+    self::$site['url'] = file_exists('.htaccess') ? $url : $url.'?/';
     self::$site['template'] = $setting['template'];
-    self::$site['template-url'] = str_replace('index.php', '', 'http://'.$domain.$_SERVER['PHP_SELF'].'square/templates/'.$setting['template'].'/');
+    self::$site['template-url'] = $url.'square/templates/'.$setting['template'].'/';
 
     $input = Helpers::get_url_input();
 
